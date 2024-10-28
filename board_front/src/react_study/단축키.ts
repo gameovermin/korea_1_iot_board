c8c8cc768c7d5c572d2aaab16822a9d2075b678b
gameovermin <qorkrhvms@gmail.com>
2024-10-28 11:49:18 +0900

    frontend project 올리기

---
+++ board_front/src/react_study/단축키.ts
@@ -0,0 +1,25 @@
+export const tmp = "";
+
+/*  
+  ! 단축키.ts  
+*/
+
+/*
+  - 터미널 열기 : Ctrl + Shift + `(백틱)
+  - 줄 복사 : Alt + Shift + 방향키(위/아래)
+
+  - 들여쓰기 : tab
+  - 내어쓰기 : Shift + tab
+
+  - 사이드바 토글 : Ctrl + B
+
+  - 검색 : Ctrl + f
+
+
+  ? << 리액트 함수형 컴포넌트 생성 단축키 >>
+  : rfc / rfce
+
+  ? 리액트 실행 명령어
+  : npm run start
+
+*/
